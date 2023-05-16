e2930485734f82bf1dd5054ffba6e375c40fc857
Kim Su Min <105275495+usgnim@users.noreply.github.com>
2023-05-17 00:50:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int dp[11];
+int main(void) {
+	int T;
+	scanf("%d", &T);
+	
+	int n;
+	dp[0] = 1;
+	dp[1] = 2;
+	dp[2] = 4;
+	
+	for(int i = 0; i < T; i++){
+		scanf("%d", &n);
+		for(int j = 3; j < n; j++){
+			dp[j] = dp[j-3] + dp[j-2] + dp[j-1];
+		}
+		printf("%d\n", dp[n-1]);
+	}
+	
+	return 0;
+}
